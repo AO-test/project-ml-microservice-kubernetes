ab8651b2b25f724cb2daeacbba01fe75d01bfac9
EC2 Default User <ec2-user@ip-172-31-70-190.ec2.internal>
2020-06-21 10:39:39 +0000

    third

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,17 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="abdulrahmanalo/app"
+imagename="app"
+
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag $imagename $dockerpath
+
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
